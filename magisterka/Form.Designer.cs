1772a45ae376688c08ceec78fabb648fc85e401a
DamianLikszo <damian.likszo@gmail.com>
2018-03-22 19:54:57 +0100

    UI

--- magisterka/Form.Designer.cs
+++ magisterka/Form.Designer.cs
@@ -28,13 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnInfo = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.gbGraph = new System.Windows.Forms.GroupBox();
+            this.lNameProgram = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.gbAbout = new System.Windows.Forms.GroupBox();
+            this.gbAbout.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold);
+            this.btnLoad.Location = new System.Drawing.Point(12, 12);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(259, 82);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Wczytaj";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            // 
+            // btnInfo
+            // 
+            this.btnInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold);
+            this.btnInfo.Location = new System.Drawing.Point(12, 115);
+            this.btnInfo.Name = "btnInfo";
+            this.btnInfo.Size = new System.Drawing.Size(259, 82);
+            this.btnInfo.TabIndex = 0;
+            this.btnInfo.Text = "Instrukcja";
+            this.btnInfo.UseVisualStyleBackColor = true;
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold);
+            this.btnEnd.Location = new System.Drawing.Point(12, 269);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(259, 82);
+            this.btnEnd.TabIndex = 0;
+            this.btnEnd.Text = "Koniec";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            // 
+            // gbGraph
+            // 
+            this.gbGraph.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold);
+            this.gbGraph.Location = new System.Drawing.Point(300, 12);
+            this.gbGraph.Name = "gbGraph";
+            this.gbGraph.Size = new System.Drawing.Size(832, 479);
+            this.gbGraph.TabIndex = 1;
+            this.gbGraph.TabStop = false;
+            this.gbGraph.Text = "Graf:";
+            // 
+            // lNameProgram
+            // 
+            this.lNameProgram.AutoSize = true;
+            this.lNameProgram.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic);
+            this.lNameProgram.Location = new System.Drawing.Point(6, 18);
+            this.lNameProgram.MaximumSize = new System.Drawing.Size(244, 60);
+            this.lNameProgram.MinimumSize = new System.Drawing.Size(244, 60);
+            this.lNameProgram.Name = "lNameProgram";
+            this.lNameProgram.Size = new System.Drawing.Size(244, 60);
+            this.lNameProgram.TabIndex = 2;
+            this.lNameProgram.Text = "Wyznaczanie elementów sumowo nieredukowalnych oraz porządku zbiorów otwartych.";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic);
+            this.label1.Location = new System.Drawing.Point(6, 98);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "v. 1.0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Italic);
+            this.label2.Location = new System.Drawing.Point(131, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Damian Likszo";
+            // 
+            // gbAbout
+            // 
+            this.gbAbout.Controls.Add(this.label2);
+            this.gbAbout.Controls.Add(this.label1);
+            this.gbAbout.Controls.Add(this.lNameProgram);
+            this.gbAbout.Location = new System.Drawing.Point(12, 357);
+            this.gbAbout.Name = "gbAbout";
+            this.gbAbout.Size = new System.Drawing.Size(259, 134);
+            this.gbAbout.TabIndex = 3;
+            this.gbAbout.TabStop = false;
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1144, 503);
+            this.Controls.Add(this.gbAbout);
+            this.Controls.Add(this.gbGraph);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnInfo);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form";
             this.Text = "Form1";
+            this.gbAbout.ResumeLayout(false);
+            this.gbAbout.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnInfo;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.GroupBox gbGraph;
+        private System.Windows.Forms.Label lNameProgram;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox gbAbout;
     }
 }
 
