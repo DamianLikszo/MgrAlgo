8bcfad83018baa5fbb2276a31febfbb65a5ccb36
DamianLikszo <damian.likszo@gmail.com>
2018-05-27 23:54:12 +0200

    Child can have more than one Parent

--- magisterka/Models/Granula.cs
+++ magisterka/Models/Granula.cs
@@ -6,11 +6,15 @@ namespace magisterka.Models
     {
         public List<int> Inside { get; set; }
         public Granula Child { get; set; }
-        public Granula Parent { get; set; }
+        public List<Granula> Parent { get; set; }
+
+        public bool isMax => Parent.Count == 0;
+        public bool isMin => Child == null;
 
         public Granula()
         {
             Inside = new List<int>();
+            Parent = new List<Granula>();
         }
 
         public void AddToInside(int value)
