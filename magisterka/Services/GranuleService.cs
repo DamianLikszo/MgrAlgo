d646f03ba5e95edb02962c73a75a922d0b662d52
DamianLikszo <damian.likszo@gmail.com>
2018-06-11 01:01:37 +0200

    Refractore generate granule

--- magisterka/Services/GranuleService.cs
+++ magisterka/Services/GranuleService.cs
@@ -10,36 +10,27 @@ namespace magisterka.Services
         public ZbGran GenerateGran(List<List<int>> data)
         {
             var zbGran = new ZbGran();
-
-            // sprawdzenie po kórych indeksach sprawdzamy
-            List<List<int>> indexSelect = new List<List<int>>();
-            foreach (var row in data)
-            {
-                var rowIndexSelect = new List<int>();
-                for (int i = 0; i < row.Count; i++)
-                {
-                    if (row[i] == 1)
-                        rowIndexSelect.Add(i);
-                }
-                indexSelect.Add(rowIndexSelect);
-            }
-
-            // porównywanie min
-            // ? czy indexSelect moze byc 0 ?
+            
             for (int u = 0; u < data.Count; u++)
             {
                 int result;
                 var granule = new Granula();
+                var indexSelect = new List<int>();
 
+                for (int j = 0; j < data[u].Count; j++)
+                {
+                    if (data[u][j] == 1)
+                        indexSelect.Add(j);
+                }
+                
                 for (int i = 0; i < data.Count; i++)
                 {
                     result = 1;
-                    foreach (var index in indexSelect[u])
+                    foreach (var index in indexSelect)
                     {
                         if (result == 0)
                             break;
 
-                        var test = data[i][index];
                         result = Math.Min(result, data[i][index]);
                     }
                     granule.AddToInside(result);
